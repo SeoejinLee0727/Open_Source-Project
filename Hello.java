f2903c02a895cd96f2cd59b7d7a92ffb1608eee7
SeoejinLee0727 <81629443+SeoejinLee0727@users.noreply.github.com>
2022-09-20 22:54:29 +0900

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,25 @@
+
+
+public class Hello {
+	
+	public static int sum(int n, int m) {
+		return n+m;
+		// 메소드
+	}
+	
+	
+	//main() 메소드에서 실행 시작
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);  // sum() 메소드 호출
+		a = '?';
+		System.out.println(a);  // 문자 '?' 화면 출력
+		System.out.println("Hello");		
+		System.out.println(s);  // 정수 s값 
+		
+	}
+	
+}
